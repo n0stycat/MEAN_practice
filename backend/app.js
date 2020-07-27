720e582869ea8b4d50d20381d771fc2387361df7
n0sty <n0stycat@gmail.com>
2020-07-27 16:35:00 +0300

    Resolved CORS error with headers

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,35 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next()
+});
 
-app.use('/api/posts', (req, res)=> {
+app.use("/api/posts", (req, res) => {
   const posts = [
     {
-      id: 'sometypeofid',
-      title: 'First server-side post',
-      content: 'This is coming from the server'
+      id: "sometypeofid",
+      title: "First server-side post",
+      content: "This is coming from the server"
     },
     {
-      id: 'sometypeofid2',
-      title: 'Second server-side post',
-      content: 'This is coming from the server!'
+      id: "sometypeofid2",
+      title: "Second server-side post",
+      content: "This is coming from the server!"
     }
   ];
   res.status(200).json({
-    message: 'Posts fetched successfully',
+    message: "Posts fetched successfully",
     posts: posts
   });
 });
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from "@angular/core";
 import {Subject} from 'rxjs';
 import {Post} from "./post.model";
+import {HttpClient} from '@angular/common/http'
 
 
 @Injectable({
@@ -10,8 +11,15 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>()
 
+  constructor(private http: HttpClient) {
+  }
+
   getPosts() {
-    return [...this.posts]; //spread
+    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
+      .subscribe((postData) => {
+        this.posts = postData.posts;
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 
   getPostUpdateListener() {
@@ -19,8 +27,8 @@ export class PostsService {
   }
 
   addPost(title: string, content: string) {
-    const newPost: Post = {title: title, content: content};
+    const newPost: Post = {id: null, title: title, content: content};
     this.posts.push(newPost);
-    this.postsUpdated.next([...this.posts]);
+    this.postsUpdated.next([...this.posts]); //spread
   }
 }
